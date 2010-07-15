d987c38b8e9fc3937177cb34a562c6c377be4397
John Athayde <jmpa@meticulous.com>
2010-07-15 02:27:37 -0400

    date formats

---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,9 @@
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!({
+  :euro => "%d %B %Y",
+  :cc_expiration => "%m/%y",
+  :microformat => '%Y %m %d',
+  :myformat => '%d %b %Y'  
+})
+
+#ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS
+
